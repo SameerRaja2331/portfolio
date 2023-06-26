05b4dd17d1347e911991a5833549adf4c2a48ec7
SameerRaja2331 <51845839+SameerRaja2331@users.noreply.github.com>
2023-06-25 20:21:15 -0500

    Update-2.2 next config change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+//module.exports = nextConfig
+module.exports = {
+  distDir: ".next",
+  outDir: "public",
+};
